ed159d6eb6f4a3a08be1df7437f5632c9181cbac
Rohit Agarwal <programmingwithrohit@gmail.com>
2017-06-30 09:55:38 +0530

    Add files via upload

---
+++ creational/singleton/LazyInitializationSingleton.java
@@ -0,0 +1,18 @@
+package com.javamultiplex.creational.singleton;
+
+public class LazyInitializationSingleton {
+
+	private static LazyInitializationSingleton instance;
+
+	private LazyInitializationSingleton() {
+
+	}
+
+	public static LazyInitializationSingleton getInstance() {
+		if (instance == null) {
+			instance = new LazyInitializationSingleton();
+		}
+		return instance;
+	}
+
+}
