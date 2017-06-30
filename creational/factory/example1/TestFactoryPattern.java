30fbc36f470a30fca2dcc4574d01f56f369c1dd2
Rohit Agarwal <programmingwithrohit@gmail.com>
2017-06-30 09:56:57 +0530

    Add files via upload

---
+++ creational/factory/example1/TestFactoryPattern.java
@@ -0,0 +1,15 @@
+package com.javamultiplex.creational.factory.example1;
+
+public class TestFactoryPattern {
+
+	public static void main(String[] args) {
+
+		Computer pc = ComputerFactory.getComputer("pc", "4GB", "500GB",
+				"2.4GHZ");
+
+		Computer server = ComputerFactory.getComputer("server", "10GB", "1TB",
+				"10GHZ");
+		System.out.println("PC : " + pc);
+		System.out.println("Server : " + server);
+	}
+}
